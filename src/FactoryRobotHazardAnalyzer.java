35dc6acc2fb8c2cbe0f630c3b169b2b01e3ec136
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-01-29 16:07:55 +0530

    [Nehaa] : [Added] :UC4: validate inputs using if-else

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -18,13 +18,38 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter Machinery State:");
         String machineryState = sc.nextLine();
 
-        // UC3: Hazard Risk Calculation (NO validation)
-        double machineRiskFactor = 3.0; // assume Critical
+        double machineRiskFactor;
 
-        double hazardRisk =
-                ((1.0 - armPrecision) * 15.0)
-                        + (workerDensity * machineRiskFactor);
-
-        System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        // UC4: Validation using if-else
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be 0.0-1.0");
+        }
+        else if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be 1-20");
+        }
+        else if (machineryState.equals("Worn")) {
+            machineRiskFactor = 1.3;
+            double hazardRisk =
+                    ((1.0 - armPrecision) * 15.0)
+                            + (workerDensity * machineRiskFactor);
+            System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        }
+        else if (machineryState.equals("Faulty")) {
+            machineRiskFactor = 2.0;
+            double hazardRisk =
+                    ((1.0 - armPrecision) * 15.0)
+                            + (workerDensity * machineRiskFactor);
+            System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        }
+        else if (machineryState.equals("Critical")) {
+            machineRiskFactor = 3.0;
+            double hazardRisk =
+                    ((1.0 - armPrecision) * 15.0)
+                            + (workerDensity * machineRiskFactor);
+            System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        }
+        else {
+            System.out.println("Error: Unsupported machinery state");
+        }
     }
 }
